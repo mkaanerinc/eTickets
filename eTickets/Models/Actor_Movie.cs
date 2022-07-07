6be8c6a38b51d949de3f62b7b511109cf1875cdf
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2022-07-07 15:48:55 +0300

    Added Models & Database Connection

---
+++ eTickets/Models/Actor_Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Actor_Movie
+    {
+        public int MovieId { get; set; }
+        public int ActorId { get; set; }
+
+        // Relationships
+        public Movie Movie { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
