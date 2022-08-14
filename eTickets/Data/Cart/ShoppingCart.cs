9bcac255ce868c67ee1b93acd6c2c83c50a0251a
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2022-08-14 12:07:11 +0300

    Added ShoppingCart Model

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+        private string ShoppingCartId { get; set; }
+        private List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.Set<ShoppingCartItem>().Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            return _context.Set<ShoppingCartItem>().Where(x => x.ShoppingCartId == ShoppingCartId).Select(x => x.Movie.Price * x.Amount).Sum();
+        }
+    }
+}
