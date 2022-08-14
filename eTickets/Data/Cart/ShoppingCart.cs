5da51ef117acaca75ca26b2715fabc6afa4276d3
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2022-08-14 12:28:24 +0300

    Added AddItemToCart Method

--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -16,6 +16,28 @@ namespace eTickets.Data.Cart
             _context = context;
         }
 
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.Set<ShoppingCartItem>().FirstOrDefault(x => x.Movie.Id == movie.Id && x.ShoppingCartId == ShoppingCartId);
+
+            if(shoppingCartItem is null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    Movie = movie,
+                    ShoppingCartId = ShoppingCartId,
+                    Amount = 1,
+                };
+
+                _context.Set<ShoppingCartItem>().Add(shoppingCartItem);
+            } else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            _context.SaveChanges();
+        }
+
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.Set<ShoppingCartItem>().Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Movie).ToList());
